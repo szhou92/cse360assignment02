abc4ef90b25b15cd0ca1899af472ae15942d402d
siyuan <siyuan@100.64.12.131>
2020-10-05 18:23:39 -0700

    Second Version

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -3,10 +3,10 @@
  * 
  * Description: The adding machine program will perform add and 
  * subtract operations of integers from a variable and calculate 
- * the total. It can print the result by calling toString method.
+ * the total. It can print the operations by calling toString 
+ * method and print the result by calling getTotal method.
  * 
  * @author Siyuan Zhou
- * @version 1.0
  * @date 10/5/2020
  */
 
@@ -15,13 +15,19 @@ package cse360assignment02;
 public class AddingMachine
 {
 	private int total;
+	
+	private String result;
 
 	/**
-	 * Constructor of the AddingMachine
+	 * Constructor of the AddingMachine.
+	 * 
+	 * Setting total to 0 and result to "0".
 	 */
 	public AddingMachine()
 	{
 		total = 0; // not needed - included for clarity
+		
+		result = "0";
 	}
 
 	
@@ -32,39 +38,70 @@ public class AddingMachine
 	 */
 	public int getTotal()
 	{
-		return 0;
+		return total;
 	}
 
 	/**
-	 * The method adds the values.
+	 * The method adds the value to total.
 	 * 
 	 * @param value
 	 */
 	public void add(int value)
 	{
+		total = total + value;
+		
+		result = result + " + " + value;
 	}
 
 	/**
-	 * The method subtracts the values.
+	 * The method subtracts the value from total.
 	 * 
 	 * @param value
 	 */
 	public void subtract(int value)
 	{
+		total = total - value;
+		
+		result = result + " - " + value;
 	}
 
 	/**
 	 * The method will return a String of the operations and the result.
+	 * 
+	 * @return the string of operations
 	 */
 	public String toString()
 	{
-		return "";
+		return result;
 	}
 
 	/**
-	 * The method will clear and reset the values.
+	 * The method will clear the operations and reset the values.
 	 */
 	public void clear()
 	{
+		total = 0;
+		
+		result = "0";
+	}
+	
+	/**
+	 * The main will test the calculation results.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		AddingMachine myCalculator = new AddingMachine();
+		
+		myCalculator.add (4); 
+		
+		myCalculator.subtract (2); 
+		
+		myCalculator.add(5);
+		
+		System.out.println(myCalculator.toString());
+		
+		System.out.println(myCalculator.getTotal());
 	}
 }
