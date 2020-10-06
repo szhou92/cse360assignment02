93bf387ec0a27c189349e8f8d4c93072073e4237
siyuan <siyuan@100.64.12.131>
2020-10-05 17:54:38 -0700

    Initial Version

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,70 @@
+/**
+ * CSE 360 Assignment 01: Version Control
+ * 
+ * Description: The adding machine program will perform add and 
+ * subtract operations of integers from a variable and calculate 
+ * the total. It can print the result by calling toString method.
+ * 
+ * @author Siyuan Zhou
+ * @version 1.0
+ * @date 10/5/2020
+ */
+
+package cse360assignment02;
+
+public class AddingMachine
+{
+	private int total;
+
+	/**
+	 * Constructor of the AddingMachine
+	 */
+	public AddingMachine()
+	{
+		total = 0; // not needed - included for clarity
+	}
+
+	
+	/**
+	 * The method will return the total value after calculations.
+	 * 
+	 * @return total value of the calculations
+	 */
+	public int getTotal()
+	{
+		return 0;
+	}
+
+	/**
+	 * The method adds the values.
+	 * 
+	 * @param value
+	 */
+	public void add(int value)
+	{
+	}
+
+	/**
+	 * The method subtracts the values.
+	 * 
+	 * @param value
+	 */
+	public void subtract(int value)
+	{
+	}
+
+	/**
+	 * The method will return a String of the operations and the result.
+	 */
+	public String toString()
+	{
+		return "";
+	}
+
+	/**
+	 * The method will clear and reset the values.
+	 */
+	public void clear()
+	{
+	}
+}
